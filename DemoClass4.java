d9a0af8b01c5fe9f73a03b4d1f3da8067670d3fa
Bruce <brucedaniel724@gmail.com>
2024-09-18 12:12:26 +0300

    Adding multiple data to db using adBatch

---
+++ DemoClass4.java
@@ -0,0 +1,62 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * DemoClass4
+ */
+/**
+ * DemoClass4
+ */
+public class DemoClass4 {
+
+    static String url = "jdbc:mysql://localhost:3306/people";
+    static String user = "root";
+    static String password = "@8VDBHZ12724";
+    static String query = "INSERT INTO person (roll,name) VALUES (?,?)";
+
+
+  public static void main(String[] args) {
+    DemoClass4 demoClass4 = new DemoClass4();
+    demoClass4.addBatch();
+     
+    }
+    void addBatch(){
+         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(url,user,password);
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1,7);
+            statement.setString(2,"King");
+            statement.addBatch();
+
+            statement.setInt(1, 8);
+            statement.setString(2, "James");
+            statement.addBatch();
+
+            statement.setInt(1, 9);
+            statement.setString(2, "United");
+            statement.addBatch();
+
+            statement.setInt(1, 10);
+            statement.setString(2, "America");
+            statement.addBatch();
+
+            // statement.clearBatch();
+
+            int [] res = statement.executeBatch();
+
+           for (int i : res) {
+            System.out.println(i);
+           }
+            
+            con.close();
+            statement.close();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
